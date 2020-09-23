ce3f434f88c4b6cc6a1fb84b680b26fd1b583626
bbajpai <b.bhumi101@gmail.com>
2020-09-23 19:46:40 +0530

    a

---
+++ 1datatypes.js
@@ -0,0 +1,99 @@
+String
+number
+boolean
+
+string = "fvfdvfv" '43rf43g' '44232' "443" "true" 'false' '5/5/2020'
+number = 534  545446456 454.465  .4654 
+boolean = true/false
+null
+undefined
+
+////////////////////Es5/////////////////////////////////
+
+var a = "hi"
+var b = 10
+var c = true
+//////////////////////////////
+
+var a = 10
+typeof(a)
+"number"
+var b = "hi"
+typeof(b)
+"string"
+var c = true
+typeof(b)
+"string"
+typeof(c)
+"boolean"
+var d = "10"
+typeof(d)
+"string"
+
+var a = 1
+undefined
+var b = 2
+undefined
+a+b
+3(addition)
+a-b
+-1
+a*b
+2
+a/b
+0.5
+4%2
+0
+4%3
+1
+
+3%4
+3
+2%4
+2
+
+
+
+var a = "hi "
+undefined
+var b = "JavaScript"
+undefined
+a+b(concat)
+"hi JavaScript"
+a-b
+NaN
+a*b
+NaN
+a/b
+NaN > not a number
+1+"hi"
+"1hi"
+"hi"+1
+"hi1"
+
+string+string = string
+string+number = string
+number+string = string
+number+number= number
+
+10+"20"+30
+"102030"
+
+"10"+20+30
+"1020"+30
+"102030"
+
+10+20+"30"
+30+"30"
+"3030"
+
+
+10+"20"+30-1
+102029
+"10"+20+30-1
+102029
+10+20+"30"-1
+3029
+
+
+
