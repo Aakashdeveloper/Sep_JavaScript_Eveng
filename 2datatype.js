34a0e947791bfdfb8f761a42a9ba83ce94677aff
bbajpai <b.bhumi101@gmail.com>
2020-09-24 19:40:42 +0530

    A

---
+++ 2datatype.js
@@ -0,0 +1,44 @@
+var a = true
+var b = true
+
+var a = true
+var b = true
+undefined
+a+b
+2
+a+10
+11
+a+false
+1
+
+'a'+true
+"atrue"
+'true'+'true'
+"truetrue"
+true+true
+2
+
+var a = '1'
+undefined
+var b = '2'
+undefined
+a+b
+"12"
+parseInt(a)+parseInt(b)
+3
+Number(a)+Number(b)
+3
+var c = "10.44"
+undefined
+var d = "11.33"
+undefined
+c+d
+"10.4411.33"
+parseInt(c)+parseInt(d)
+21
+parseFloat(c)+parseFloat(d)
+21.77
+Number(c)+Number(d)
+21.77
+parseFloat(a)
+1
---
+++ 3promptconfirm.js
@@ -0,0 +1,34 @@
+console.log() >> for check output and debugg
+alert() > for Notification
+prompt() > to take input from user
+confirm() > to check weather yes or no 
+
+
+alert("Form submitted")
+undefined
+alert("Form submitted")
+undefined
+confirm('Do you want to leave the page')
+true
+confirm('Do you want to leave the page')
+false
+var output = confirm('Do you want to leave the page')
+undefined
+output
+true
+var output = confirm('Do you want to leave the page')
+undefined
+output
+false
+
+prompt("What is your name")
+"Aakash"
+var a = prompt("Enter first value")
+undefined
+a
+"444"
+
+var a = prompt("Enter first value");
+var b = prompt("Enter Second Value")
+var output = Number(a)+Number(b)
+alert(output)
