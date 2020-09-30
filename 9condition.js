7f16322bd4e92798151df4de9fa8452848668e60
aakashdeveloper <ahanda205@gmail.com>
2020-09-30 19:51:03 +0530

    A

---
+++ 9condition.js
@@ -0,0 +1,20 @@
+= assignment
+== we are comparing data of variable
+=== we are comparing data and datatype
+
+var a = 10
+var b = "10"
+a==b
+true
+a===b
+false
+
+//case senstive
+var name = "john"
+undefined
+var name1="John"
+undefined
+name == name1
+false
+name === name1
+false
